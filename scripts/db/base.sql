4d15294c8da8a7d87a61d8a6ffa160bff495a84d
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2022-11-30 21:53:30 +0700

    fix: docker compose and added something in base sql

--- scripts/db/base.sql
+++ scripts/db/base.sql
@@ -1,5 +1,29 @@
-CREATE DATABASE binotifyrest;
-CREATE DATABASE binotifydb;
+CREATE DATABASE IF NOT EXISTS binotifyrest;
+CREATE DATABASE IF NOT EXISTS binotifydb;
+CREATE DATABASE IF NOT EXISTS binotifysoap;
+
+USE binotifysoap;
+
+CREATE TABLE IF NOT EXISTS Logging (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    description varchar(256) NOT NULL,
+    IP varchar(16) NOT NULL,
+    endpoint varchar(256) NOT NULL,
+    requested_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS Subscription (
+    creator_id INT,
+    subscriber_id INT,
+    status ENUM('PENDING', 'ACCEPTED', 'REJECTED') NOT NULL DEFAULT 'PENDING',
+    PRIMARY KEY(creator_id, subscriber_id)
+);
+
+CREATE TABLE IF NOT EXISTS ApiKeys (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    service_name varchar(256) NOT NULL,
+    api_key varchar(256) NOT NULL UNIQUE
+);
 
 USE binotifydb;
 
